546efa4cbcaffaad54e6dc851b9435b9b608eac2
Qwerty2727 <qwerty27027@gmail.com>
2015-11-17 12:43:03 -0500

    Simple Update
    Refines the music and menus

--- Xi/Assets/Standard Assets/Scripts/ChangeMusic.cs
+++ Xi/Assets/Standard Assets/Scripts/ChangeMusic.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class ChangeMusic : MonoBehaviour
 {
-	
+	public AudioClip menuMusic;
 	public AudioClip level2Music;
-	
+	public AudioClip level3Music;
+	public AudioClip level4Music;
 	
 	private AudioSource source;
 	
@@ -19,10 +20,26 @@ public class ChangeMusic : MonoBehaviour
 	
 	void OnLevelWasLoaded(int level)
 	{
-		if (level == 2)
+		switch (level)
 		{
+		case 1:
+			source.clip = menuMusic;
+			source.Play ();
+			break;
+		case 3:
 			source.clip = level2Music;
 			source.Play ();
+			break;
+		case 4:
+			source.clip = level3Music;
+			source.Play ();
+			break;
+		case 5:
+			source.clip = level4Music;
+			source.Play ();
+			break;
+		default:
+			break;
 		}
 	}
 }
